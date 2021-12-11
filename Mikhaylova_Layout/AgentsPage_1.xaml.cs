e7a6ccd226ac24d66fa87f8f9a0a0445b7d88ec2
mmetrin <6mikhaylovakatya9@gmail.com>
2021-12-11 16:31:11 +0700

    Добавьте файлы проекта.

---
+++ Mikhaylova_Layout/AgentsPage_1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mikhaylova_Layout
+{
+    /// <summary>
+    /// Логика взаимодействия для AgentsPage_1.xaml
+    /// </summary>
+    public partial class AgentsPage_1 : Page
+    {
+        public AgentsPage_1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnNext_Click(object sender, RoutedEventArgs e)
+        {
+         
+        }
+
+        private void BtnLast_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
